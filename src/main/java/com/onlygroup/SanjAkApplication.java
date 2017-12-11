078e8b44205bce18636b8947b2987ca7b4148c7e
Akshay <Akshay@10.0.2.15>
2017-12-11 13:38:20 +0530

    Fir

---
+++ src/main/java/com/onlygroup/SanjAkApplication.java
@@ -0,0 +1,12 @@
+package com.onlygroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanjAkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanjAkApplication.class, args);
+	}
+}
