556bd4275cf0d5be57f38694d48bbb4c33bbf5c9
Akshay <Akshay@10.0.2.15>
2017-12-11 13:42:17 +0530

    s

--- src/main/java/com/onlygroup/SanjAkApplication.java
+++ src/main/java/com/onlygroup/SanjAkApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SanjAkApplication {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(SanjAkApplication.class, args);
 	}
